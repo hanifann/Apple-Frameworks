60273fc7de2a1954fa47c57db3194d404b468e0f
hanifan <hanifannurulhaq@outlook.com>
2023-11-03 19:33:33 +0700

    add frameworks model and mock data

---
+++ Apple-Frameworks/FrameworksGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworksGridView.swift
+//  Apple-Frameworks
+//
+//  Created by hanifan nurul haq on 03/11/23.
+//
+
+import SwiftUI
+
+struct FrameworksGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworksGridView()
+}
