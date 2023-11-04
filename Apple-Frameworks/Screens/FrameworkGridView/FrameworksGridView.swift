edcec570da6c0c4868e7d32ec8ebd33aeb2d54d8
hanifan <hanifannurulhaq@outlook.com>
2023-11-04 21:13:02 +0700

    refactor into a separate folder

--- Apple-Frameworks/Screens/FrameworkGridView/FrameworksGridView.swift
+++ Apple-Frameworks/Screens/FrameworkGridView/FrameworksGridView.swift
@@ -11,14 +11,10 @@ struct FrameworksGridView: View {
     
     @StateObject var viewModel = FrameworkGridViewModel()
     
-    let columns: [GridItem] = [GridItem(.flexible()),
-                               GridItem(.flexible()),
-                               GridItem(.flexible())]
-    
     var body: some View {
         NavigationView {
             ScrollView {
-                LazyVGrid(columns: columns){
+                LazyVGrid(columns: viewModel.columns){
                     ForEach(MockData.frameworks){ framework in
                         FrameworkTitleView(framework: framework)
                             .onTapGesture {
@@ -39,23 +35,4 @@ struct FrameworksGridView: View {
 
 #Preview {
     FrameworksGridView()
-}
-
-struct FrameworkTitleView: View{
-    
-    let framework: Framework
-    
-    var body: some View {
-        VStack {
-            Image(framework.imageName)
-                .resizable()
-                .frame(width: 90, height: 90)
-            Text(framework.name)
-                .font(.title2)
-                .fontWeight(.semibold)
-                .scaledToFit()
-                .minimumScaleFactor(0.5)
-        }
-        .padding()
-    }
 }
